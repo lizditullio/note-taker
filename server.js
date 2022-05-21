ab1e2c9c1aa92775034957d2da63bf55dccef11d
Liz DiTullio <lizdit72296@gmail.com>
2022-05-21 13:58:08 -0400

    get and post routes working

--- server.js
+++ server.js
@@ -24,39 +24,52 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
-app.get('/api/get-all', (req, res) => {
+app.get('/api/notes', (req, res) => {
     const database = readDatabase();
     res.json(database)
 });
 
-app.post('/api/add-lead', (req, res) => {
-    const newLead = {...req.body}
-    newLead.id = crypto.randomUUID();
-
+app.post('/api/notes', (req, res) => {
     const database = readDatabase();
+    
+   let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: crypto.randomUUID(),
+    }
 
-    database.push(newLead);
+    database.push(newNote);
 
     writeToDatabase(database);
 
-    res.json(newLead);
+    res.json(newNote);
 });
 
-app.put('/api/update-lead/:id', (req, res) => {
+app.put('/api/notes:id', (req, res) => {
     const database = readDatabase()
 
     for (let i = 0; i < database.lenght; i++) {
-        const lead = database[i]
+        const note = database[i]
 
-        if(lead.id == req.params.id) {
-            
+        if(note.id == req.params.id) {
+            note.complete = req.body.complete 
+            writeToDatabase(database);
+            res.status(204);
         }
     }
+    res.status(404).end()
 })
 
-app.delete('/api/delete-lead:id', (req, res) => {
-    
+app.delete('/api/notes:id', (req, res) => {
+    const database = readDatabase();
+    const newData = database.filter((lead) => lead.id != req.params.id);
+    if(database.lenght == newData.length) {
+        res.status(404).end;
+    }
+    writeToDatabase(newData);
+    res.status(200).end
 });
+
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`)
